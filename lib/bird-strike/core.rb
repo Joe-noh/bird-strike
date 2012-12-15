5535e0c4907385e7ff37ad0057688b1aed9f7f97
Joe-noh <goflb.jh@gmail.com>
2012-12-15 23:23:17 +0900

    deleted some comments

--- lib/bird-strike/core.rb
+++ lib/bird-strike/core.rb
@@ -7,9 +7,8 @@ module BirdStrike
     def initialize
       Curses.init
 
-      home_win = Curses::Window.new(0, 0, 0, 0)
-      home_win.puts_title
-#      @prompt = Prompt.new(home_win.maxy)
+      @windows = [Curses::Window.new(0, 0, 0, 0)]
+      @windows.first.puts_title
 
       sleep 1
 
@@ -17,13 +16,14 @@ module BirdStrike
       keys   = FileIO.get_consumer_key_secret
       token  = FileIO.get_access_token
 
-      if token.nil?  # Authorization Part
-        home_win.print_center(Authorization.get_oauth_url keys, -4)
+      # Authorization Part
+      if token.nil?
+        @windows.first.print_center(Authorization.get_oauth_url keys, -4)
         begin
-          pin   = home_win.prompt("pin").strip.to_i
+          pin   = @windows.first.prompt("pin").strip.to_i
           token = Authorization.get_oauth_token(pin)
         rescue => e
-          home_win.print_center(e.message, -2)
+          @windows.first.print_center(e.message, -2)
           retry
         end
         FileIO.store_access_token token
@@ -31,16 +31,16 @@ module BirdStrike
       Authorization.authorize(keys.merge token)
       @@stream_client = TweetStream::Client.new
 
-      @timelines = [new_stream(home_win, :userstream)]
+      @timelines = [new_stream(@windows.first, :userstream)]
 
-      Thread.new(@timelines){ |timelines|  # Input Processing
-        timelines.each{|tl| tl.stream.run}
+      # Input Processing
+      Thread.new(@timelines){ |timelines|
         loop do
           cmd = Curses.getch
           case cmd
-          when 13 || Curses::KEY_ENTER  # Enter
+          when 13 || Curses::Key::ENTER  # Enter
             timelines.first.stop
-            prompt = Prompt.new(home_win.maxy)
+            prompt = Prompt.new(Curses.stdscr.maxy)
             tweet = prompt.get_input
             Twitter.update(tweet) unless tweet.nil?
             prompt.close
@@ -49,8 +49,6 @@ module BirdStrike
           end
         end
       }.join
-
-      #[TwitterModule::Streaming.thread, InputLoop.thread].each(&:run)
     end
 
     def new_stream(window, method, *args)
@@ -58,7 +56,7 @@ module BirdStrike
       tl.stream = Thread.new {
         @@stream_client.send(method, *args, &tl.on_receipt)
       }
-#      tl.stream.run
+      tl.stream.run
       return tl
     end
 

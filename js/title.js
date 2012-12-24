ffe6eab5b85a53d4fb5dfdec05bb47f50da2c1a9
Joe-noh <goflb.jh@gmail.com>
2012-12-24 18:32:50 +0900

    update

--- js/title.js
+++ js/title.js
@@ -1,18 +1,22 @@
-var canvas = document.getElementById("title");
-var context = canvas.getContext("2d");
 
-var point = {x:0, y:20};
-var timer;
+window.onload = function(){
+    var canvas = document.getElementById("title");
+    var context = canvas.getContext("2d");
 
-function draw(x, y){
-    context.clearRect(0, 0, 900, 200);
-    context.fillRect(x, y, 10, 10);
-}
+    var point = {x:0, y:20};
+    var timer;
 
-var loop = function(){
-    point.x = point.x + 2;
-    point.y = point.y + 2;
-    draw(point.x, point.y);
-    clearTimeout(timer);
-    timer = setTimeout(timer, 50);
+    function draw(x, y){
+	context.clearRect(0, 0, 900, 200);
+	context.fillRect(x, y, 10, 10);
+    }
+
+    var loop = function(){
+	point.x = point.x + 2;
+	point.y = point.y + 2;
+	draw(point.x, point.y);
+	clearTimeout(timer);
+	timer = setTimeout(timer, 50);
+    }
+    loop();
 }

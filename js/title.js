40a14de03f0aa38a8a0c1882964881941570f2e2
Joe-noh <goflb.jh@gmail.com>
2012-12-24 18:23:57 +0900

    update

---
+++ js/title.js
@@ -0,0 +1,19 @@
+var canvas = document.getElementById("title");
+var context = canvas.getContext("2d");
+
+var point = {x:0, y:20};
+var timer;
+
+function draw(x, y){
+    context.clearRect(0, 0, 900, 200);
+    context.fillRect(x, y, 10, 10);
+}
+
+var loop = function(){
+    point.x += 2;
+    point.y += 2;
+    draw(point.x, point.y);
+    clearTimeout(timer);
+    timer = setTimeout(timer, 50);
+}
+loop();
